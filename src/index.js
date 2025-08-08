5c401161cd9b6188ec11d7e1361fc49036201c85
Pallavi-jain74 <pallaviumadi88@gmail.com>
2025-08-08 11:08:47 +0530

    initial comit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Import the Express module
+import http from 'http';
+import app from './app.js'
+
+let PORT=3000;
+let server=http.createServer(app)
+
+server.listen(PORT,()=>{
+    console.log(`Server is on port ${PORT}`);
+    
+})
+
